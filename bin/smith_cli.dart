44a5636140c7e3252403b3392564767f569b58be
nasrul_alawy <alawy.nasrul@gmail.com>
2023-03-06 11:02:44 +0800

    fix: smith command

--- bin/smith_cli.dart
+++ bin/smith_cli.dart
@@ -3,20 +3,18 @@ import 'package:smith_cli/smith_cli.dart' as smith_cli;
 import 'dart:io';
 
 void main(List<String> arguments) {
-  // Cek apakah command yang dimasukkan adalah "smith -g all event"
-  if (arguments.length >= 2 && arguments[0] == "smith") {
-    // Panggil fungsi generateEventCode()
-    if (arguments[1] == "-g" || arguments[1] == "generate") {
-      if (arguments[2] == "all") {
-        smith_cli.generateDataDataSource(arguments[3]);
-        smith_cli.generateDataRepository(arguments[3]);
-        smith_cli.generateDomainRepositoryInterface(arguments[3]);
-        smith_cli.generateDomainService(arguments[3]);
-        smith_cli.generateDomainUsecase(arguments[3]);
-      }
+  // Cek apakah command yang dimasukkan adalah "smith -g all"
+  // Panggil fungsi generateEventCode()
+  if (arguments[0] == "-g" || arguments[0] == "generate") {
+    if (arguments[1] == "all") {
+      smith_cli.generateDataDataSource(arguments[2]);
+      smith_cli.generateDataRepository(arguments[2]);
+      smith_cli.generateDomainRepositoryInterface(arguments[2]);
+      smith_cli.generateDomainService(arguments[2]);
+      smith_cli.generateDomainUsecase(arguments[2]);
     }
   } else {
     // Jika command yang dimasukkan salah, tampilkan pesan error
-    print("Command not recognized. Usage: smith -g all event");
+    print("Command not recognized. Usage: smith -g all [className]");
   }
 }
